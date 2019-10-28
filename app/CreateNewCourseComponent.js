5eeea5439cb1d7a7cbe18f82788d1eca5deae8dd
Navit <navit@navits-imac.home>
2019-10-29 01:16:51 +0530

    Added React UI for ceating new course

--- app/CreateNewCourseComponent.js
+++ app/CreateNewCourseComponent.js
@@ -2,25 +2,147 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import { Redirect } from "react-router-dom";
 import { Divider, Modal, Form, FormField, Dropdown, Radio, FormButton, TextArea, FormCheckbox, Checkbox, Button, Grid, FormGroup, FormTextArea, Segment, Message} from 'semantic-ui-react';
+import TimePicker from 'rc-time-picker';
+import 'rc-time-picker/assets/index.css';
+import moment from 'moment';
+import DatePicker from "react-datepicker";
+import 'react-datepicker/dist/react-datepicker.css';
 
+const DatePickerComponent = (props) => {
+    const [ date, setDate ] = useState();
+    return (
+      <DatePicker selected={date} onChange={date => {let d = moment(date).utc().format("YYYY-MM-DD HH:mm:ss");setDate(date); props.onChange(d)}} />
+    );
+};
 
 export default function CreateNewCourseComponent(props){
 
     const [ open, setOpen ] = useState(false);
     const [ formRef ] = useState(React.createRef());
+    const [ formSubmittedId, setFormSubmittedId ] = useState(0);
     
     const [ courseName, setCourseName ] = useState("");
     const [ courseCode, setCourseCode ] = useState("");
     const [ courseDesc, setCourseDesc ] = useState("");
     const [ tAEmail, setTaEmail ] = useState("");
     const [ tAName, setTaName ] = useState("");
+    const [ startDate, setStartDate ] = useState()
+    const [ endDate, setEndDate ] = useState()
+    const [ classStartTime, setClassStartTime ] = useState(moment().format("hh:mm"))
+    const [ classEndTime, setClassEndTime ] = useState(moment().format("hh:mm"))
     
     const [ courseCreated, setCourseCreated ] = useState(false);
     const [ okayHandled, setOkHandled ] = useState(false);
+    const [ okayType, setOkType ] = useState("action");
     const [ feedbackModalOpen, setFeedbackModalOpen ] = useState(false);
+    const [ feedbackModalMessage, setFeedbackModalMessage ] = useState(false);
+
+    const [ ignoreWarnings, setIgnoreWarnings ] = useState(false);
+
+    // console.log("startDate", startDate)
+    // console.log("endDate", endDate)
+    // console.log("classStartTime", classStartTime)
+    // console.log("classEndTime", classEndTime) // DEBUG
 
     useEffect(() => {
-        setFeedbackModalOpen(true);
+        if (formSubmittedId == 0) return;
+        console.log("Handle Submit called"); 
+        // console.log(startDate);
+        // console.log(endDate);
+        // console.log(classStartTime);
+        // console.log(classEndTime);
+        // console.log(courseName);
+        // console.log(courseCode);
+        // console.log(courseDesc);
+        // console.log(tAEmail);
+        // console.log(tAName); // DEBUG
+        let warningMessages = ""
+        if (!courseCode){
+            setFeedbackModalMessage("Course Code cannot be empty.")
+            setOkType("invalid")
+            setFeedbackModalOpen(true);
+        }else if (!courseName){
+            setFeedbackModalMessage("Course Name cannot be empty.")
+            setOkType("invalid")
+            setFeedbackModalOpen(true);
+        }
+        else{
+            if (!courseDesc){
+                if (!ignoreWarnings){
+                    warningMessages += "Note: You have not set any description for the course\n"
+                }
+            }
+            if (!tAEmail){
+                if (!ignoreWarnings){
+                    warningMessages += "You have not set any email address for the course's Teaching Assistant\n"
+                }
+            }
+            if (!tAName){
+                if (!ignoreWarnings){
+                    warningMessages += "You have not set a name for the course's Teaching Assistant"
+                }
+            }
+            if (!ignoreWarnings && (warningMessages != "")){
+                setOkType("warn")
+                setFeedbackModalMessage(warningMessages);
+                setFeedbackModalOpen(true);
+            }else{
+                // Try to create the course using the server API
+                sendCreateRequest();
+            }
+        }
+    },[formSubmittedId])
+
+    const sendCreateRequest = async function(){
+        console.log("Trying to CREATE COURSE")
+        let postBody = {
+            "course_code": courseCode,
+            "course_name": courseName,
+            "course_description": courseDesc,
+            "ta_name": tAName,
+            "ta_email": tAEmail,
+            "professor_id": 1,
+            "start_date": startDate,
+            "end_date": endDate,
+            "class_start_time": classStartTime + ":00",
+            "class_end_time": classEndTime + ":00"
+        }
+        try{
+            let response = await fetch("http://localhost:3000/professor/1/course/save", {
+                method: 'POST',
+                headers:{
+                    'Content-Type': 'application/json'
+                },
+                cache: 'no-cache',
+                body: JSON.stringify(postBody)
+            })
+            console.log(JSON.stringify(postBody))
+    
+            let responseJson = await response.json()
+            if (responseJson){
+                if (responseJson.status == "ok"){
+                    console.log(responseJson)
+                    setOkType("success")
+                    setFeedbackModalMessage("Course created successfully");
+                    setFeedbackModalOpen(true);
+                }else{
+                    setFeedbackModalMessage((responseJson.reason && `${responseJson.reason}`) || (responseJson.errorFull && `${responseJson.errorFull}`) || "Problem creating the new course");
+                    setFeedbackModalOpen(true);
+                }
+            }else{
+                setFeedbackModalMessage("Problem creating the new course");
+                setFeedbackModalOpen(true);
+            }
+        }catch(error){
+            console.log(error)
+            // setFeedbackModalMessage(error.message);
+            setFeedbackModalMessage(error.message + " " + JSON.stringify(error));
+            setFeedbackModalOpen(true);
+        }
+    }
+
+    useEffect(() => {
+        setFeedbackModalOpen(false);
     },[courseCreated])
 
     useEffect(() => {
@@ -29,19 +151,18 @@ export default function CreateNewCourseComponent(props){
 
     return (
         <div>
-        {okayHandled && (<Redirect to={"/course"}/>)}
-        {(!courseCreated) &&
+        {courseCreated && (<Redirect to={"professor/1/course"}/>)}
+        {
             (<Modal 
                 trigger={<Button onClick={() => setOpen(true)} fluid>Create New Course</Button>}
                 open={open}
             >
-
                 <Modal.Header>
                     {props.header || "Create New Course"}
                 </Modal.Header>
                 <Modal.Content scrolling>
-                    <Form ref={formRef} onSubmit={() => {console.log("Handle Submit called"); console.log(`courseName:${courseName}, courseCode:${courseCode}, TA Name:${tAName}, TA Email:${tAEmail} `); setCourseCreated(true)}}>
-                            <FormField 
+                    <Form ref={formRef} onSubmit={() => setFormSubmittedId(formSubmittedId+1)}>
+                            <FormField
                                 label={"Course Name"}
                                 placeholder={"Enter Course Name"}
                                 value={courseName}
@@ -78,6 +199,34 @@ export default function CreateNewCourseComponent(props){
                                 value={tAEmail}
                                 onChange={(e) => setTaEmail(e.target.value)}
                             />
+                            <FormGroup
+                                style={{ alignItems:"center" }}
+                            >
+                                <FormField
+                                    label={"Class Start Time"}
+                                    placeholder={"Enter Teaching Assistant's Email ID"}
+                                />
+                                <TimePicker
+                                    showSecond={false}
+                                    defaultValue={moment()}
+                                    onChange={(value) => setClassStartTime(value.format('HH:mm'))}
+                                />
+                                <FormField
+                                    label={"Class End Time"}
+                                    placeholder={"Enter Teaching Assistant's Email ID"}
+                                />
+                                <TimePicker
+                                    showSecond={false}
+                                    defaultValue={moment()}
+                                    onChange={(value) => setClassEndTime(value.format('HH:mm'))}
+                                />  
+                            </FormGroup>
+                            <FormGroup
+                                style={{ alignItems:"center" }}
+                            >
+                                <DatePickerComponent onChange={setStartDate}/>
+                                <DatePickerComponent onChange={setEndDate}/>
+                            </FormGroup>
                     </Form>
                 </Modal.Content>
                 <Modal.Actions>
@@ -93,12 +242,29 @@ export default function CreateNewCourseComponent(props){
                                         }>Cancel</Button>
                 </Modal.Actions>
             </Modal>)}
-        {courseCreated && <Modal open={feedbackModalOpen}>
-                <Modal.Content>{"Couse Created Successfully"}</Modal.Content>
+        {<Modal open={feedbackModalOpen}>
+                <Modal.Content>{feedbackModalMessage}</Modal.Content>
                 <Modal.Actions>
-                        <Button onClick={() => setOkHandled(true)}>
-                            Ok
+                        <Button onClick={() => {
+                                if (okayType == "success"){
+                                    setCourseCreated(true)
+                                }else if (okayType == "inmotion"){
+                                    setFeedbackModalMessage("");
+                                    setFeedbackModalOpen(false)
+                                }else{
+                                    setFeedbackModalMessage("");
+                                    setFeedbackModalOpen(false)
+                                }
+                            }
+                        }>
+                            {okayType == "warn" ? "Cancel" : "Ok"}
                         </Button>
+                        {
+                            okayType == "warn" && 
+                            <Button positive onClick={() => {setIgnoreWarnings(true);formRef.current.handleSubmit()}}>
+                                Create
+                            </Button>
+                        }
                 </Modal.Actions>
             </Modal>}
         </div>
