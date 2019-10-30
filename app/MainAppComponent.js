24afedc6ae9fd776cd7ecfe7643817dca176d44e
Navit <navit@navits-imac.home>
2019-10-30 07:03:51 +0530

    Modified app to include global sidebar and header

--- app/MainAppComponent.js
+++ app/MainAppComponent.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {useState} from 'react';
 import {
     BrowserRouter as Router,
     Switch,
@@ -9,21 +10,18 @@ import {
 import {default as DisplayCourseComponent} from "./DisplayCourseComponent";
 import {default as RosterDetails} from "./DisplaySavedRosterComponent";
 import {default as DisplayPickedFile} from "./DisplayPickedFile";
-import { Table, Header, Cell } from 'semantic-ui-react';
+import { Table, Header, Cell, Icon, Button, Grid, GridColumn } from 'semantic-ui-react';
 import {default as SurveyFormQuestionComponent} from "./SurveyFormQuestionComponent";
+import {default as SidebarComponent } from "./SidebarComponent";
+import { default as HeaderComponent } from "./AppHeaderComponent";
 
 
 
 export default function MainAppComponent() {
-    var courseDetails = {
-        name:"SSDI Fall'19",
-        classCode:"ITCS 6112 - 8112",
-        proffessorName:"Harini Ramaprasad",
-        taName:"Manav Mittal",
-        courseStartDate:"August 19, 2019",
-        courseEndDate:"December 12, 2019",
-        courseDescription:"This course is about teaching software programming principles and Software architecture and design."
-    }
+
+    const [ sidebarVisible, setSidebarVisible ] = useState(false);
+    const [ sidebarButtonRef ] = useState(React.createRef());
+
 
     const HeaderCellComponent = (props) => (<Table.HeaderCell singleLine>{props.headerName}</Table.HeaderCell>);
     var getHeaders = () => {
@@ -35,32 +33,52 @@ export default function MainAppComponent() {
         return ret;
     }
     const headers = getHeaders();
+    const appBackGroundStyle = {
+        background:"linear-gradient(0deg,rgba(0,0,0,0.7),rgba(0,0,0,0.7)),url(/assets/images/background.jpg)"
+    }
+    
 
     return (
+        <div>
         <Router>
-            <Link to="/course"/>
-            <Link to="/"/>
-            <Switch>
-                <Route exact={true} path={`/professor/1/course/1/roster/9`}>
-                    <RosterDetails>{headers}</RosterDetails>
-                </Route>
-                <Route 
-                    exact={true} 
-                    path={"/professor/1/course/survey/add"}>
-                        <SurveyFormQuestionComponent />
-                </Route>
-                <Route 
-                    exact={true} 
-                    path={"/professor/1/course/chooseroster/view"}>
-                        <DisplayPickedFile />
-                </Route>
-                <Route exact={true}  path="/professor/1/course">
-                    <DisplayCourseComponent {...courseDetails}/>
-                </Route>
-                <Route path="/">
-                    <DisplayCourseComponent {...courseDetails}/>
-                </Route>
-            </Switch>
+            <Route path="/">
+                <SidebarComponent setVisible={setSidebarVisible} visible={sidebarVisible} pushStyle={appBackGroundStyle}>
+                    <Grid style={{ background:"inherit"}}>
+                        <Grid.Row>
+                            <HeaderComponent setSidebarVisible={setSidebarVisible}/>
+                        </Grid.Row>
+                        <Grid.Row>
+                            <GridColumn width={1}/>
+                            <GridColumn width={14}>
+                                <Switch>
+                                    <Route exact={true} path={`/professor/1/course/1/roster/9`}>
+                                        <RosterDetails>{headers}</RosterDetails>
+                                    </Route>
+                                    <Route 
+                                        exact={true} 
+                                        path={"/professor/1/course/survey/add"}>
+                                            <SurveyFormQuestionComponent />
+                                    </Route>
+                                    <Route 
+                                        exact={true} 
+                                        path={"/professor/1/course/chooseroster/view"}>
+                                            <DisplayPickedFile />
+                                    </Route>
+                                    <Route exact={true}  path="/professor/1/course">
+                                        <DisplayCourseComponent/>
+                                    </Route>
+                                    <Route path="/">
+                                        <DisplayCourseComponent/>
+                                    </Route>
+                                    <DisplayCourseComponent/>
+                                </Switch>
+                            </GridColumn>
+                            <GridColumn width={1}/>
+                        </Grid.Row>
+                    </Grid>
+                </SidebarComponent>
+            </Route>
         </Router>
+        </div>
     );
 }
