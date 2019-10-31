b83270b02427235dae1d98b7325f909216a9d382
ngrover2 <55921077+ngrover2@users.noreply.github.com>
2019-10-31 06:12:15 +0530

    Update MainAppComponent.js

--- app/MainAppComponent.js
+++ app/MainAppComponent.js
@@ -15,7 +15,6 @@ import { Table, Header, Cell, Icon, Button, Grid, GridColumn, Container } from '
 import {default as SurveyFormQuestionComponent} from "./SurveyFormQuestionComponent";
 import {default as SidebarComponent } from "./SidebarComponent";
 import { default as HeaderComponent } from "./AppHeaderComponent";
-import { default as RedirectComponent} from "./RedirectComponent";
 
 
 
