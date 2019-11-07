82428bf84979a3c6061344144f3d82af5239c717
Rahul Sundaresan <s.rahul@protonmail.com>
2019-11-07 10:01:53 -0500

    added debugging statements

--- app/AnswerSurveyComponent.jsx
+++ app/AnswerSurveyComponent.jsx
@@ -2,9 +2,18 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import { Form, Dropdown, Radio, FormButton, TextArea, FormCheckbox, Checkbox, Button, Grid, FormGroup, FormTextArea, Segment, Message } from 'semantic-ui-react';
+import {
+    BrowserRouter as Router,
+    Route,
+    Switch,
+    useParams
+  } from 'react-router-dom';
+  
+  
 
 export default function AnswerSurveyComponent(props){
-    const  survey_id = useParams();
+    const  { survey_id } = useParams();
     console.log(survey_id);
+    console.log("yeet");
     return (<h3>test</h3>);
 }
