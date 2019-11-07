d0cf426c5f95f06b71be7ed872c72806bda932d8
Rahul Sundaresan <s.rahul@protonmail.com>
2019-11-07 15:07:25 -0500

    Add header with deadline

--- app/StudentAnswerSurvey/AnswerSurveyComponent.jsx
+++ app/StudentAnswerSurvey/AnswerSurveyComponent.jsx
@@ -1,31 +1,14 @@
 // Component to create new Survey.
 import React from 'react';
-import { useState, useEffect } from 'react';
 import {
   Container,
-  Divider,
-  Dropdown,
-  Grid,
-  Header,
-  Image,
-  List,
   Menu,
-  Segment,
 } from 'semantic-ui-react';
-
-import {
-    BrowserRouter as Router,
-    Route,
-    Switch,
-    useParams
-  } from 'react-router-dom';
-  
-  
-  
+import { useParams } from 'react-router-dom';
 
 export default function AnswerSurveyComponent(props){
   const response = {
-    "survey_details":{
+      "survey_details":{
         "course_name":"SSDI",
         "course_instructor":"Harini",
         "deadline":1893139865
@@ -52,33 +35,24 @@ export default function AnswerSurveyComponent(props){
                 "question":"what times are you free?"
             }  
     }
-}
-
-
-const date = new Date(response.deadline*1000);
-const hours = date.getHours();
-const minutes = "0" + date.getMinutes();
-const seconds = "0" + date.getSeconds();
-var humanReadableDeadline = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
-
-
-  const  { survey_id } = useParams();
-  console.log(survey_id);
-  return (
-    <div>
-      <Menu fixed='top' inverted>
-        <Container>
-          <Menu.Item as='a' header>
-            {response.survey_details.course_name}
-          </Menu.Item>
-          <Menu.Item as='a'>
-            {response.survey_details.course_instructor}
-          </Menu.Item>
-          <Menu.Item as='a'>
-            {humanReadableDeadline}
-          </Menu.Item>
-        </Container>
-      </Menu>
-    </div>
+  };
+  const deadline = new Date(response.survey_details.deadline*1000);// deadline is in UNIX timestamp. We convert it to JS date object
+  const  { surveyId } = useParams();
+    return (
+      <div>
+        <Menu fixed='top' inverted>
+          <Container>
+            <Menu.Item as='a' header>
+              {response.survey_details.course_name}
+            </Menu.Item>
+            <Menu.Item as='a'>
+              {response.survey_details.course_instructor}
+            </Menu.Item>
+            <Menu.Item as='a'>
+              Deadline : {deadline.toString()}
+            </Menu.Item>
+          </Container>
+        </Menu>
+      </div>
   );
 }
