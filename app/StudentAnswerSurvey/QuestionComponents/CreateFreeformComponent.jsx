7a4278da2d6d5acff9a2dad87e6295ead3cc1881
Rahul Sundaresan <s.rahul@protonmail.com>
2019-11-09 18:28:47 -0500

    create working freeform component

--- app/StudentAnswerSurvey/QuestionComponents/CreateFreeformComponent.jsx
+++ app/StudentAnswerSurvey/QuestionComponents/CreateFreeformComponent.jsx
@@ -1,10 +1,7 @@
 // File that returns a component for a freeform question
+import React from 'react';
 import { Form } from 'semantic-ui-react';
 export default function FreeformComponent(props) {
-    return(
-        <div>
-            <Form.TextArea label = {props.question} />
-        </div>
-    );
+    return <Form.TextArea label={props.question} />
 
 }
