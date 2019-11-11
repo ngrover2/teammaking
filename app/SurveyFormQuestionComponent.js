5afedc4a962c7fe3d3f2c0135fbdd91db1924766
Navit <navit@navits-imac.home>
2019-11-11 07:06:15 +0530

    This can create questions of four types and render them

--- app/SurveyFormQuestionComponent.js
+++ app/SurveyFormQuestionComponent.js
@@ -1,69 +1,219 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import { Form, Dropdown, Radio, FormButton, TextArea, FormCheckbox, Checkbox, Button, Grid, FormGroup, FormTextArea, Segment, Message } from 'semantic-ui-react';
-import { default as TextQuestion } from "./TextAreaQuestionComponent";
+import { Grid, Segment, Button } from 'semantic-ui-react';
+import { default as SelectQuestionType} from "./SelectQuestionTypeRadioComponent";
+import { default as CreateTextQuestion } from "./CreateTextTypeQuestionComponent";
+import { default as CreateMCQuestionComponent} from "./CreateMCTypeQuestionComponent";
+import { default as CreateMulValuesQuestionComponent } from "./CreateMulValuesQuestionComponent";
+import { default as CreateSingleChoiceQuestionComponent } from "./CreateSingleChoiceQuestionComponent";
+
+import { default as RenderedTextQuestionComponent} from "./RenderedTextQuestionComponent";
+import { default as RenderedMCQuestionComponent} from "./RenderedMCQuestionComponent";
+import { default as RenderedMulValuesQuestionComponent} from "./RenderedMulValuesQuestionComponent";
+import { default as RenderedSingleChoiceQuestionComponent } from "./RenderedSingleChoiceQuestionComponent";
+
+import { FullWidthDivider, GridRowMessageComponent, DatePickerGridRowComponent } from "./UtilComponents";
+
+const mockQuestionObj = {
+    qid:1,
+    qtype:"text",
+    qtext:"test question",
+    qweight:"LittleImportant"
+}
 
 
 export default function SurveyFormQuestionComponent(props) {
 
-    const [ questionText, setQuestionText ] = useState("");
-    const [ numberElementsInForm, setNumberElementsInForm ] = useState(0);
+    const [ displayedQuestionComponents, setDisplayedQuestionComponents ] = useState(<Grid.Row columns={16}><Grid.Column width={16}><Segment key={"mock-message"}>No Questons Added Yet</Segment></Grid.Column></Grid.Row>); // Placeholder content when there have been no questions added yet.
+    const [ displayedQuestionObjects, setDisplayedQuestionObjects ] = useState([mockQuestionObj]); // For holding rendered questions (i.e. question components created from questions data we have so far)
+
+    const [ questionUpdatedId, setQuestionUpdatedId ] = useState(0); // For side effects on new Question creation  OR existing question modification
+
+    const [ newQuestionType, setNewQuestionType ] = useState("text"); // Track new question's answer type
+
+    const[ currentQuestionId, setCurrentQuestionId ] = useState([mockQuestionObj].length); // Track if a new question is being created
+
+    const[ clearInputsId, setClearInputsId ] = useState(0); // Track if a new question is being created
     
-    const [ addQuestionComponentReference, setAddQuestionComponentReference ] = useState();
-    const [ addQuestionComponentType, setAddQuestionComponentType ] = useState("text"); 
+    const[ surveyDeadline, setSurveyDeadline ] = useState(); // Track survey deadline date
 
-    const [ displayedQuestions, setDisplayedQuestions ] = useState([(<Segment key={numberElementsInForm}>No Questons Added Yet</Segment>)]);
-    const [ newQuestionAddedId, setNewQuestionAddedId ] = useState(0);
 
-    const [ displayRendered, setDisplayRendered ] = useState(true);
+    useEffect(() => {
+        if (questionUpdatedId == 0 && currentQuestionId == 0) return
+        // console.log("displayedQuestionObjects",displayedQuestionObjects);
+        if (displayedQuestionObjects.length == 0){
+            setDisplayedQuestionComponents(<Segment key={"mock-message"}>No Questons Added Yet</Segment>);
+        }
+        // console.log("questionUpdatedId changed, new question must have been added") // DEBUG
+        let newDisplayedQuestionComponents = [];
+        [...displayedQuestionObjects].reverse().forEach((v,i) => {
+            if (v.qtype == "text"){
+                newDisplayedQuestionComponents.push(
+                    <RenderedTextQuestionComponent 
+                        key={`q-${v.qid}`}
+                        qid={v.qid}
+                        qseqno={i}
+                        qtext={`${v.qtext}`}
+                        qweight={v.qweight}
+                        qtype={v.qtype}
+                        onDeleteClick={(questionid) => deleteQuestionObject(questionid)}
+                        onQuestionUpdated={(qobj) => updateQuestionObject(qobj)}
+                    />
+                );
+            }else if (v.qtype == "multiplechoice"){
+                newDisplayedQuestionComponents.push(
+                    <RenderedMCQuestionComponent
+                        key={`q-${v.qid}`}
+                        qid={v.qid}
+                        qtext={`${v.qtext}`}
+                        qseqno={i}
+                        choices={v.choices || []}
+                        qweight={v.qweight}
+                        qtype={v.qtype}
+                        onDeleteClick={(questionid) => deleteQuestionObject(questionid)}
+                        onQuestionUpdated={(qobj) => updateQuestionObject(qobj)}
+                    />
+                );
+            }else if (v.qtype == "multiplevalues"){
+                newDisplayedQuestionComponents.push(
+                    <RenderedMulValuesQuestionComponent
+                        key={`q-${v.qid}`}
+                        qid={v.qid}
+                        qtext={`${v.qtext}`}
+                        qseqno={i}
+                        qweight={v.qweight}
+                        qMaxNumValues={v.qMaxVals}
+                        qtype={v.qtype}
+                        onDeleteClick={(questionid) => deleteQuestionObject(questionid)}
+                        onQuestionUpdated={(qobj) => updateQuestionObject(qobj)}
+                    />
+                );
+            }else if (v.qtype == "singlechoice"){
+                newDisplayedQuestionComponents.push(
+                    <RenderedSingleChoiceQuestionComponent
+                        key={`q-${v.qid}`}
+                        qid={v.qid}
+                        qtext={`${v.qtext}`}
+                        qseqno={i}
+                        choices={v.choices || []}
+                        defaultChoice={v.defaultChoice}
+                        defaultSelected={v.defaultSelected}
+                        qweight={v.qweight}
+                        qtype={v.qtype}
+                        onDeleteClick={(questionid) => deleteQuestionObject(questionid)}
+                        onQuestionUpdated={(qobj) => updateQuestionObject(qobj)}
+                    />
+                );
+            }
+        });
+        setDisplayedQuestionComponents(newDisplayedQuestionComponents);
+    },[questionUpdatedId])
 
-    const lastRef = React.createRef();
-
-    const TextAreaFormComponent = (props) => {
-        return (
-            <Grid.Row columns={12}>
-                <Grid>
-                    <Grid.Row columns={12}>
-                        <Message>{props.message}</Message>
-                    </Grid.Row>
-                    <Grid.Row columns={12}>
-                        <Segment>
-                            <TextArea />
-                        </Segment>
-                    </Grid.Row>
-                </Grid>
-            </Grid.Row>
-        );
+    const deleteQuestionObject = (questionId) => {
+        console.log(`deleteQuestionObject called for ${questionId}`);
+        let remaining = [...displayedQuestionObjects].filter((v,i) => v.qid != questionId);
+        // let remaining = [...displayedQuestionObjects.slice(0,questionId), ...displayedQuestionObjects.slice(questionId + 1, displayedQuestionObjects.length)]
+        setDisplayedQuestionObjects(remaining);
+        setQuestionUpdatedId(questionUpdatedId+1);
     }
 
-    useEffect(() => {
-        console.log(`Question ID updated to ${newQuestionAddedId}, so use effect called`);
-        if (newQuestionAddedId == 0){
+    const updateQuestionObject = (questionObj) => {
+        // console.log("received", questionObj); DEBUG
 
-        }
-        else if (newQuestionAddedId == 1){
-            let currentQuestions = [...displayedQuestions].slice(1);
-            currentQuestions.push(<TextAreaFormComponent key={numberElementsInForm} message={`Question # ${numberElementsInForm} ${questionText}`} />);
-            setDisplayedQuestions(currentQuestions);
-            setNumberElementsInForm(numberElementsInForm+1);
+        let existingQids = [...displayedQuestionObjects].map((v,i) => v.qid );
+        if (existingQids.includes(questionObj.qid)){
+            // question with the same qid already exists
+            let updatedObjects = []
+            displayedQuestionObjects.forEach((v,i) => {
+                if (v.qid == questionObj.qid){
+                    updatedObjects.push(questionObj);
+                }else{
+                    updatedObjects.push(v);
+                }
+            });
+            setDisplayedQuestionObjects(updatedObjects);
         }else{
-            let currentQuestions = [...displayedQuestions].slice(0);
-            currentQuestions.push(<TextAreaFormComponent key={numberElementsInForm} message={`Question # ${numberElementsInForm} ${questionText}`}></TextAreaFormComponent>);
-            setDisplayedQuestions(currentQuestions);
-            setNumberElementsInForm(numberElementsInForm+1);
+            setDisplayedQuestionObjects([...displayedQuestionObjects, questionObj]);
         }
-    },[newQuestionAddedId])
+        setQuestionUpdatedId(questionUpdatedId+1);
+    }
 
-    useEffect(() => {
-        console.log(`setAddQuestionComponentType updated to ${addQuestionComponentType}, so use effect called`);
-        setAddQuestionComponentReference(<TextQuestion setQuestionText={setQuestionText} setDisplayRendered={setDisplayRendered} setNewQuestionAddedId={setNewQuestionAddedId}/>);
-    }, [setAddQuestionComponentType])
+    const getQuestionCreatorComponent = () => {
+        // console.log("newquestion id passed to createquestion is",currentQuestionId);
+        switch(newQuestionType){
+            case "text": {
+                return (
+                    <CreateTextQuestion
+                        qid={currentQuestionId}
+                        qtype="text"
+                        onQuestionUpdated={updateQuestionObject}
+                        setNewQuestionId={incrementQuestionId}
+                        clearInputsId={clearInputsId}
+                    />
+                );
+            }
+            case "multiplechoice": {
+                return (
+                    <CreateMCQuestionComponent
+                        qid={currentQuestionId}
+                        qtype="multiplechoice"
+                        clearInputsId={clearInputsId}
+                        onQuestionUpdated={updateQuestionObject}
+                        setNewQuestionId={incrementQuestionId}
+                    />)
+            }
+            case "multiplevalues": {
+                return (
+                    <CreateMulValuesQuestionComponent
+                        qid={currentQuestionId}
+                        qtype="multiplevalues"
+                        onQuestionUpdated={updateQuestionObject}
+                        setNewQuestionId={incrementQuestionId}
+                    />
+                );
+            }
+            case "singlechoice": {
+                return (
+                    <CreateSingleChoiceQuestionComponent
+                        qid={currentQuestionId}
+                        qtype="singlechoice"
+                        onQuestionUpdated={updateQuestionObject}
+                        setNewQuestionId={incrementQuestionId}
+                    />
+                );
+            }
+            default:return
+        }
+    }
+
+    const clearInputs = () => {
+        console.log("clearInputs called to increment clearInputsId:",clearInputsId);
+        setClearInputsId(clearInputsId+1);
+    }
+
+    const readyAddNewQuestion = (newType) => {
+        setCurrentQuestionId(currentQuestionId + 1);
+        if (newType && newType != "") setNewQuestionType(newType);
+    }
+
+    const incrementQuestionId = () => {
+        setCurrentQuestionId(currentQuestionId + 1);
+    }
 
     return (
             <Grid columns={12}>
-                {displayRendered && displayedQuestions}
-                <Grid.Row columns={12} key={"addQuestionComponent"}>
-                    {addQuestionComponentReference && addQuestionComponentReference}
+                <SelectQuestionType readyAddNewQuestion={(type) => readyAddNewQuestion(type)} clearInputs={clearInputs}/>
+                <GridRowMessageComponent message={"Construct your question below"}/>
+                {getQuestionCreatorComponent()}
+                <FullWidthDivider/>
+                <GridRowMessageComponent size="small" message={"All questions created by you will appear below"}/>
+                <DatePickerGridRowComponent key={`survey-pick-deadline`} onChange={setSurveyDeadline}/>
+                {displayedQuestionComponents.length > 0 && displayedQuestionComponents || (<Grid.Row columns={16}><Grid.Column width={16}><Segment key={"mock-message"}>No Questons Added Yet</Segment></Grid.Column></Grid.Row>)}
+                <Grid.Row columns={16}>
+                    <Grid.Column width={16}><Button fluid onClick={() => console.log({deadline: surveyDeadline,questions:displayedQuestionObjects})}>Save Survey</Button></Grid.Column>
+                </Grid.Row>
+                <Grid.Row columns={16} style={{minHeight:"100px"}}>
+                    <Grid.Column width={16} />
                 </Grid.Row>
             </Grid>
     );
