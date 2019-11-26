83df97e7e8802f1755417c0239c55f44a6b47867
Devika1309 <55635259+Devika1309@users.noreply.github.com>
2019-11-26 07:33:32 -0500

    added test for saveStudentResponseController

---
+++ app/tests/saveStudentResponseController.test.js
@@ -0,0 +1,49 @@
+{/**/}
+import React from 'react';
+import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
+import StudentResponseComponent from '.\API\saveStudentResponseController.js'
+
+var StudentResponseSurveyComponentDetails = {
+    "survey_answers":{
+        "studentID":"1",
+        "surveyID": "2"
+    },
+    "answer_object":
+    {
+        "1":
+            {
+                "type":"freeform",
+                "answer":"1.yes"
+            },
+        "2":{    
+                "type":"freeform",
+                "answer":"1.python"
+            },
+    }
+}; 
+
+
+function IsJsonString(str) {
+    try {
+        var sanitized = '[' + data.replace(/}{/g, '},{') + ']';
+        var res = JSON.parse(sanitized);
+
+        JSON.parse(str);
+    } catch (e) {
+        return false;
+    }
+    return true;
+}
+
+it('renders correctly', () => {
+    if  (IsJsonString(StudentResponseSurveyComponentDetails) == true) {
+    
+        const tree = renderer.create(
+            <StudentResponseComponent {...StudentResponseSurveyComponentDetails}/>
+          ).toJSON();
+          expect(tree).toMatchSnapshot();
+    
+    }
+
+});
